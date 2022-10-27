376404bb2cea0e26efac42b2dd77a910da0c22a4
ascriptur <112026384+ascriptur@users.noreply.github.com>
2022-10-27 17:38:45 -0400

    created player and enemy objects. added a floor, and physics

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    
+    private Vector2 movement;
+    private Rigidbody2D Rigidbody Component;
+
+
+    void Update()
+    {
+        
+    }
+}
