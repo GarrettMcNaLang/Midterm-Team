a6f52d8d9e99c71e13d86840ac79c7f6d842f2eb
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2022-10-27 19:14:02 -0400

    MainScripts

---
+++ Assets/Script/ScoreScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public class ScoreTrigger : MonoBehaviour
+    {//the static keyword makes it shared between all instances of this script
+        static int score = 0;
+        public TextMeshProUGUI Score;
+
+
+         if()
+            {
+
+                score++;
+                Scoretext.text = "Score" + score.ToString();
+                //is the same as saying
+                //score = score + 1;
+                //and increases the value of score by 1
+
+                Debug.Log(transform.name + " : " + score);
+            }
+        }
+    }
--- Assets/Script/StartButton.cs
+++ Assets/Script/StartButton.cs
@@ -5,15 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void LoadLevel(string GameScene)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(GameScene);
     }
 }
