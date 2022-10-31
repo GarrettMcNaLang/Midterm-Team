68de5260464e757da24b865b6c76e65da7a1aec3
ascriptur <112026384+ascriptur@users.noreply.github.com>
2022-10-31 16:14:05 -0400

    updated playerscript and added projectiles and sprite layers

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
+/// <summary>
+/// Player controller and behavior
+/// </summary>
 public class PlayerScript : MonoBehaviour
 {
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
     public Vector2 speed = new Vector2(50, 50);
 
+    // 2 - Store the movement and the component
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
 
     void Update()
     {
-       
-        float InputX = Input.GetAxis("Horizontal");
-        float InputY = Input.GetAxis("Vertical");
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(speed.x * InputX, speed.y * InputY, 0);
-
-        movement *= Time.deltaTime;
-
-        transform.Translate(movement);
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
 
     }
 
+    void FixedUpdate()
+    {
+        // 5 - Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // 6 - Move the game object
+        rigidbodyComponent.velocity = movement;
+    }
 }
