744f88dc721a6a6bad055471495ca92702c8eccd
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2022-11-01 22:05:23 -0400

    some UI work

--- Assets/Script/ScoreScript.cs
+++ Assets/Script/ScoreScript.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 
 public class ScoreScript : MonoBehaviour
 {//the static keyword makes it shared between all instances of this script
-       // static int score = 0;
-       // public TextMeshProUGUI Score;
+        public float score;
+       public TextMeshProUGUI Scoretext;
+
+    private void Update()
+    {
+        score = Time.deltaTime();
+    }
 
 
-         //if()
-            //{
-
-                //score++;
-                //Scoretext.text = "Score" + score.ToString();
-                //is the same as saying
-                //score = score + 1;
-                //and increases the value of score by 1
-
-                //Debug.Log(transform.name + " : " + score);
-            }
-   // }
+}
+   
