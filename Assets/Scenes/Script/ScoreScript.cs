033ecd783dad9eb1cece4199c53920ee15be329e
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2022-11-02 13:15:00 -0400

    some more files/think I'm almost done

--- Assets/Scenes/Script/ScoreScript.cs
+++ Assets/Scenes/Script/ScoreScript.cs
@@ -8,11 +8,17 @@ public class ScoreScript : MonoBehaviour
         public float score;
        public TextMeshProUGUI Scoretext;
 
-    private void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
+        //collides with the "ground"
     {
-        score = Time.deltaTime();
+        score++;
+        Scoretext.text = "SCORE:" + score.ToString();
+    //will count the score as the player is on the ground.
+
+        if(score.On)
     }
-
-
 }
+
+
+
    
